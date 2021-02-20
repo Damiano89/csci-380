d9cd9dd44e4785dac9392e6e7b2ab181fe603081
Damiano Canali <canali.damiano@yahoo.com>
2021-02-20 13:19:39 -0500

    function added to show random MadLibs with buttons

--- 2-counter/app.js
+++ 2-counter/app.js
@@ -1,8 +1,12 @@
 // set inital value to zero
-let count = 2;
+let count = 0;
 // select value and buttons
-const value = document.querySelector("#value");
+const outputDiv = document.querySelector("#output");
 const btns = document.querySelectorAll(".btn");
+
+const companies = ["Uber", "Lyft", "Google", "Apple", "SpaceX", "Tinder"]
+const animals = ["Aardvark", "Blue Footed Booby", "Flying Dragon", "Giraffe Weevil", "Hammerhead Slug", "Komodo Dragon", "Naked Mole Rat", "Okapi", "Red Panda"]
+const vowels = ["A", "E", "I", "O", "U"]
 
 console.log(btns)
 
@@ -10,28 +14,25 @@ console.log(btns)
 //   // btn.addEventListener()
 // }
 
+function addCompany(){
+  const companyName = companies[Math.floor(Math.random() * companies.length)]
+  const firstLetter = companyName.charAt(0)
+  const companyString = "My company is a" + ((vowels.indexOf(firstLetter) > -1) ? "n " : " ") + companyName + " for " + animals[Math.floor(Math.random() * animals.length)] + "s!<br />"
+  outputDiv.innerHTML = companyString
+}
 
 btns.forEach(function (btn) {
   btn.addEventListener("click", function (e) {
     const styles = e.currentTarget.classList;
-    if (styles.contains("decrease")) {
-      count--;
-    } else if (styles.contains("increase")) {
-      count *= 2;
-      // count = count * 2
-    } else {
-      count = 0;
-    }
 
-    if (count > 0) {
-      value.style.color = "green";
+    if (styles.contains("decrease")) {
+          addCompany()  
+    } else if (styles.contains("increase")) {
+          addCompany() 
+    } else {
+      const companyString = " "
+      outputDiv.innerHTML = companyString
+      
     }
-    if (count < 0) {
-      value.style.color = "red";
-    }
-    if (count === 0) {
-      value.style.color = "#222";
-    }
-    value.textContent = count;
   });
 });
